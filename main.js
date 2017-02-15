be8182a6027c02017711c3fc4997642db143cbe1
Mati <matiborlak@gmail.com>
2017-02-15 11:50:50 +0200

    step 1 and 2, list off posts with id

---
+++ main.js
@@ -0,0 +1,30 @@
+/*
+
+*/
+var $posts = $(".posts");
+var $postText = $("#post-name");
+var identity =0;
+
+var posts = [];
+
+var createPost = function (text){
+	newPost = {
+		"data-text": text,
+		"data-id": identity
+	};
+	identity +=1;
+	posts.push(newPost);
+}
+
+var updatePosts = function () {
+	$posts.empty();
+	for(i=0;i<posts.length;i++) {
+		$posts.append("<p class='posts' data-id='"+posts[i]['data-id']+"'>"+"<a href='#' class='remove'>Remove-</a>"+" \""+posts[i]['data-text']+"\""+"</p>");
+	}
+}
+
+$(".btn").on("click", function() {
+	createPost($postText.val());
+	updatePosts();
+	$postText.val("");
+});
