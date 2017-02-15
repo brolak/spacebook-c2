ef67276543f015b2dd0617010bd0045f2c99c98b
Mati <matiborlak@gmail.com>
2017-02-15 17:01:48 +0200

    comment button working

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 var $posts = $(".posts");
 var $postText = $("#post-name");
+var $commentText = $(".comments")
 var identity =0;
 var removeId=0;
+var user = "USERNAME";
 
 var posts = [];
 
@@ -13,7 +15,14 @@ var createPost = function (text){
 	};
 	identity +=1;
 	posts.push(newPost);
-}
+};
+
+var createComment = function (text){
+	newComment = {
+		"text": text,
+		"id": user
+	}
+};
 
 var removePost = function () {
 	for(var i = 0; i < posts.length; i++) {
@@ -22,24 +31,31 @@ var removePost = function () {
         	break;
     	}
 	}
-}
+};
 
 var updatePosts = function () {
 	$posts.empty();
 	for(i=0;i<posts.length;i++) {
-		$posts.append("<p class='posts' data-id='"+posts[i].id+"'>"+"<a href='#' class='remove'>Remove-</a>"+" \""+posts[i].text+"\""+"</p><div class='comments'><input type='text' class='comment' placeholder='Add a comment!'></input></div><br>");
+		$posts.append("<p class='posts' data-id='"+posts[i].id+"'>"+"<a href='#' class='remove'>Remove-</a>"+"<strong>"+user+"</strong>: \""+posts[i].text+"\""+"</p>");
+		$posts.append("<div class='comments'><input type='text' class='comment' placeholder='Add a comment!'></input><button type='button' class='btn-md btn-primary comment-button'>Post</button></div><br>")
 	}
-}
+};
 
-$(".btn").on("click", function() {
+$(".add-post").on("click", function() {
 	createPost($postText.val());
 	updatePosts();
 	$postText.val("");
 });
 
+$(".posts").on("click",".comment-button", function() {
+	
+	$comment = $(this).find("input");
+	alert($comment.val());
+	$(".comments").append("<p>\"Example\"</p>");
+});
+
 $("div .posts").on("click",".remove", function () {
 	removeId= $(this).parent().attr("data-id");
-	$(this).parent().remove();
 	removePost();
 	updatePosts();
-})
+});
